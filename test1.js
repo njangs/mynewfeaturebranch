246ab16f659a487dfdb87e53c9473174159d3f57
naveen jangam <jangam.naveen@hotmail.com>
2023-02-12 20:38:43 -0600

    Version 7

--- test1.js
+++ test1.js
@@ -1,2 +1,3 @@
 console.log("test1.version5");
 console.log("test1.update this to check if the file changes are tracked or not");
+console.log("this change is from the new-git-tutorial folder");
--- test2.js
+++ test2.js
@@ -1,1 +1,2 @@
 console.log("just to check updated to check somethign else")
+console.log ("this is the next update from new git tutorial repo")
